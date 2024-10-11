b5e1efa253adb1b1b37b2e5305468cbe0cd1f81b
dungpham <dung.pham@silkwires-dev.com>
2024-10-11 10:07:45 +0700

    update hover

---
+++ _nuxt/DYL5uCLg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./JghWGhgM.js","./C6hTUVlg.js","./vue.BQqch1fn.css","./BLVB0qGI.js","./B7LlgBWf.js","./entry.Bqqn71Ok.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./C6hTUVlg.js";const r=()=>e(()=>import("./JghWGhgM.js").then(t=>t.e),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url).then(t=>t.default);r();
