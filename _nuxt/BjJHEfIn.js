d33d14c165b0a342c1b2373fc51d56a08f9eaf83
dungpham <dung.pham@silkwires-dev.com>
2024-10-15 10:08:46 +0700

    update color

--- _nuxt/BjJHEfIn.js
+++
@@ -1,6 +0,0 @@
-import{bJ as t,b4 as e,b2 as n}from"./PQ9VTHH8.js";const r={},a={class:"absolute inset-0 blur-xl h-screen",style:{background:`linear-gradient(
-				143.6deg,
-				rgba(192, 132, 252, 0) 20.79%,
-				rgba(232, 121, 249, 0.26) 40.92%,
-				rgba(204, 171, 238, 0) 70.35%
-			)`}};function s(c,o){return e(),n("div",a)}const _=t(r,[["render",s]]);export{_ as default};
--- _nuxt/aqtkMH9Y.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./BBm5LsJ6.js","./PQ9VTHH8.js","./vue.BQqch1fn.css","./BLVB0qGI.js","./B7LlgBWf.js","./entry.oyS2TJgQ.css"])))=>i.map(i=>d[i]);
-import{_ as e}from"./PQ9VTHH8.js";const r=()=>e(()=>import("./BBm5LsJ6.js").then(t=>t.e),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url).then(t=>t.default);r();
