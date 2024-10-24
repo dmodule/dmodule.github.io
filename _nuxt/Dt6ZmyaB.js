6f5caf4df0a762c01a95e86852bab0d7438658cf
dungpham <kimthiendung@gmail.com>
2024-10-24 20:51:58 +0700

    up

---
+++ _nuxt/Dt6ZmyaB.js
@@ -0,0 +1,7 @@
+import{bP as t,bs as a,bq as e}from"./C-QD9eoP.js";const n={},s={class:"absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg",style:{background:`linear-gradient(
+				106.89deg,
+				rgba(192, 132, 252, 0.11) 15.73%,
+				rgba(14, 165, 233, 0.41) 15.74%,
+				rgba(232, 121, 249, 0.26) 56.49%,
+				rgba(79, 70, 229, 0.4) 115.91%
+			)`}};function r(c,o){return a(),e("div",s)}const b=t(n,[["render",r]]);export{b as default};
